3406f3fc404f500500cae41738b042e1af0546ce
Haha64142 <paul.w64142@gmail.com>
2025-07-14 02:36:34 -0500

    Add WoodCodeUtils namespace
    Add namespace WoodCodeUtils with functions escapeChar and escapeString for
    better error text.
    Change the convertToBase and getDate functions from WoodCode to WoodCodeUtils.

--- include/woodcode.h
+++ include/woodcode.h
@@ -4,6 +4,21 @@
 #include <unordered_map>
 #include <string>
 
+namespace WoodCodeUtils
+{
+    // Converts a non-printable char into it's escaped version ('\n' -> "\\n")
+    std::string escapeChar(char c);
+
+    // Runs escapeChar() on each character
+    std::string escapeString(const std::string &str);
+
+    // Converts a number to the given base and returns result as an int
+    int convertToBase(int num, int base);
+
+    // Gets the current date in MMDDYY format
+    std::string getDate();
+}
+
 class WoodCode
 {
 public:
@@ -14,13 +29,13 @@ public:
     bool initialized = false;
 
     // Public encode function
-    std::string encode(std::string input);
+    std::string encode(std::string input) const;
 
     // Public decode function
-    std::string decode(std::string input);
+    std::string decode(std::string input) const;
 
     // Returns a string of valid characters (For the help menu)
-    std::string getValidChars();
+    std::string getValidChars() const;
 
 private:
     std::unordered_map<char, int> charMap;
@@ -30,22 +45,16 @@ private:
     std::unordered_map<int, char> specialReverseMap;
 
     // Validates input string and returns first invalid character if found
-    std::optional<char> checkInput(std::string &input);
-
-    // Gets the current date in MMDDYY format
-    std::string getDate();
+    std::optional<char> checkInput(std::string &input) const;
 
     // Calculates date offset using base-7 conversion logic
-    int getDateOffset(std::string &date);
+    int getDateOffset(std::string &date) const;
 
     // Encodes data with the given date offset
-    std::string encodeData(std::string &input, int &dateOffset);
+    std::string encodeData(std::string &input, int &dateOffset) const;
 
     // Decodes data with the given date offset
-    std::string decodeData(std::string &input, int &dateOffset);
-
-    // Converts a number to the given base and returns result as an int
-    int convertToBase(int num, int base);
+    std::string decodeData(std::string &input, int &dateOffset) const;
 
     // Initializes the character map from key and value strings
     bool initCharMap(const std::string &keys, const std::string &values);
